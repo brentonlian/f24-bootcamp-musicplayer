ebc1cd63441833717494e73dfe3180e6a98fda2b
Brenton Lian <blian@unc.edu>
2024-09-24 16:27:51 -0400

    Final commit

---
+++ f24-bootcamp-musicplayer/f24-bootcamp-musicplayer/PlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerView.swift
+//  f24-bootcamp-musicplayer
+//
+//  Created by Brenton on 9/24/24.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerView()
+}
---
+++ f24-bootcamp-musicplayer/f24-bootcamp-musicplayer/f24_bootcamp_musicplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  f24_bootcamp_musicplayerApp.swift
+//  f24-bootcamp-musicplayer
+//
+//  Created by Brenton on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct f24_bootcamp_musicplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
